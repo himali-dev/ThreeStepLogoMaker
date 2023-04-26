3dfdd20f3da97fc87adf91eceea3be6cd417e3d2
radhey-molsoft <himali.baps@gmail.com>
2023-04-25 22:49:21 -0400

    square class added

--- ref/shapes.js
+++ ref/shapes.js
@@ -16,3 +16,8 @@ class Circle extends Shape{
         return `<circle cx="100%" cy="100%" r="200" height="100%" width="100%" fill="${this.color}"/>`
     }
 }
+class Square extends Shape{
+    render(){
+        return `<rect x="100" height="400" width="400" fill="${this.color}"/>`
+    }
+}
