fa73539cb965038d63226412e8ee79dd6ab1afd9
radhey-molsoft <himali.baps@gmail.com>
2023-04-25 22:50:27 -0400

    triangle added

--- ref/shapes.js
+++ ref/shapes.js
@@ -11,13 +11,23 @@ class Shape{
     }
 
 
-class Circle extends Shape{
+
+
+    class Square extends Shape{
+        render(){
+            return `<rect x="100" height="400" width="400" fill="${this.color}"/>`
+        }
+    }
+    class Circle extends Shape{
     render(){
         return `<circle cx="100%" cy="100%" r="200" height="100%" width="100%" fill="${this.color}"/>`
     }
 }
-class Square extends Shape{
+class Triangle extends Shape{
     render(){
-        return `<rect x="100" height="400" width="400" fill="${this.color}"/>`
+
+        return `<polygon height="100%" width="100%" points="0,400 600,400 300,0" fill="${this.color}"/>`
     }
-}
+};
+
+module.exports = {Circle, Square, Triangle}
